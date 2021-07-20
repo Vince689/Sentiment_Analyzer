45bbad5ee15c51d85bbfe8af9a07ca58fd96dee6
Vincent <vince23.ac@gmail.com>
2021-07-20 07:25:35 +0300

    updated app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def preProcess_data(text): #cleaning the data
 
 app = FastAPI()
 
-data = pd.read_csv('archive/Sentiment.csv')
+data = pd.read_csv('C:/Git-projects/Git/Sentiment.csv')
 tokenizer = Tokenizer(num_words=2000, split=' ')
 tokenizer.fit_on_texts(data['text'].values)
 
@@ -47,7 +47,7 @@ def take_inp():
 @app.post('/predict') #prediction on data
 def predict(text:str = Form(...)): #input is from forms
     clean_text = my_pipeline(text) #cleaning and preprocessing of the texts
-    loaded_model = tf.keras.models.load_model('sentiment.h5') #loading the saved model
+    loaded_model = tf.keras.models.load_model('C:/Git-projects/Git/sentiment.h5') #loading the saved model
     predictions = loaded_model.predict(clean_text) #making predictions
     sentiment = int(np.argmax(predictions)) #index of maximum prediction
     probability = max(predictions.tolist()[0]) #probability of maximum prediction
